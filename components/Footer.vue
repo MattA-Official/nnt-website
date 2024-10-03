b1f1ede95dc7f5aab26db3668a85ccc71a332ad9
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2024-10-03 15:11:41 +0100

    Basic site layout

---
+++ components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer>
+        <nav>
+            <ul>
+                <li v-for="link in data.body" :key="link.url">
+                    <nuxt-link :to="link.url">{{ link.text }}</nuxt-link>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+</template>
+
+<script setup>
+const { data } = await useAsyncData('footer', () => queryContent('/_data/footer').findOne())
+</script>
+
+<style scoped>
+footer {
+    background-color: #333;
+    color: #fff;
+    padding: 1rem;
+}
+
+nav ul {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+}
+
+nav a {
+    color: #fff;
+    text-decoration: none;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li v-for="link in data.body" :key="link.url">
+                    <nuxt-link :to="link.url">{{ link.text }}</nuxt-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <!-- <p>{{ data }}</p> -->
+</template>
+
+<script setup>
+const { data } = await useAsyncData('header', () => queryContent('/_data/header').findOne());
+</script>
+
+<style scoped>
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 1rem;
+}
+
+nav ul {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+}
+
+nav a {
+    color: #fff;
+    text-decoration: none;
+}
+</style>
