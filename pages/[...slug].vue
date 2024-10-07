57c6047c210531f6416084a13d443d9392c60ea1
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2024-10-07 14:25:51 +0100

    Better 404 page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,25 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc>
+      <template #not-found>
+        <div id="not-found">
+          <h1>404 - Page Not Found</h1>
+          <p>The page you are looking for does not exist.</p>
+        </div>
+      </template>
+      <template #empty>
+        <h1>Nothing has been written for this page yet!</h1>
+      </template>
+    </ContentDoc>
   </main>
 </template>
+
+<style scoped>
+#not-found {
+  max-width: var(--page-max-width);
+  width: 100%;
+  margin: 0 auto;
+  padding: 1rem 5rem;
+  text-align: center;
+}
+</style>
