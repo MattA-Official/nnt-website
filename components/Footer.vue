73c134b24ea1312d41445c5d7051bc10bd551bd8
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2024-10-10 15:52:01 +0100

    Add social media links to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,16 +2,21 @@
     <footer>
         <hr />
         <div class="container">
-            <ul>
-                <!-- TODO: socials will go here -->
+            <ul class="socials-list">
+                <li v-for="link in data.socials" :key="link.url">
+                    <a :href="link.url">
+                        <Icon class="icon" :name="'icon:' + link.icon" v-if="link.icon" />
+                        <span v-else class="missing-icon">Icon Missing</span>
+                    </a>
+                </li>
             </ul>
             <div class="legal">
                 <p>&copy; The Nottingham New Theatre, {{ new Date().getFullYear() }}</p>
-                <p>Proudly part of the Staging Change Network</p>
+                <p><em>Proudly part of the Staging Change Network</em></p>
             </div>
             <nav>
                 <ul>
-                    <li v-for="link in data.body" :key="link.url">
+                    <li v-for="link in data.links" :key="link.url">
                         <nuxt-link :to="link.url">{{ link.text }}</nuxt-link>
                     </li>
                 </ul>
@@ -39,6 +44,7 @@ footer {
     padding: 4rem;
     width: 100%;
     justify-content: space-between;
+    justify-items: center;
 }
 
 .legal {
@@ -47,6 +53,8 @@ footer {
     gap: .5rem;
     justify-content: center;
     text-align: center;
+    width: auto;
+    margin: 0;
 }
 
 ul {
@@ -54,6 +62,7 @@ ul {
     display: flex;
     padding: 0;
     gap: 1rem;
+    width: 14ch
 }
 
 nav ul {
@@ -63,6 +72,17 @@ nav ul {
     padding: 0;
     gap: .4rem;
     text-align: right;
+}
+
+.socials-list {
+    font-size: 1.5rem;
+    align-items: center;
+}
+
+.missing-icon {
+    font-size: 0.6rem;
+    width: min-content;
+    display: inline-block;
 }
 
 /* Can be modified as needed for different screen sizes */
