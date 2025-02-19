4c8b73f9d5f85e2b4324101bc5fecd3c9cc3a36a
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2025-02-19 00:09:43 +0000

    Fix Table of Contents styling and layout

--- components/content/ui/TableOfContents.vue
+++ components/content/ui/TableOfContents.vue
@@ -21,12 +21,45 @@ defineProps({
 
 <style scoped>
 .table-of-contents {
-  position: sticky;
-  top: 0;
-  padding: 2rem;
-  background: var(--color-primary);
-  color: var(--color-white);
+  padding: 1.5rem;
+  background: var(--header-bg-color);
+  border: 1px solid var(--nnt-orange);
   border-radius: 0.5rem;
-  margin-left: auto;
+  font-size: 0.9rem;
+}
+
+.table-of-contents ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.table-of-contents ul ul {
+  padding-left: 1rem;
+  margin-top: 0.5rem;
+}
+
+.table-of-contents li {
+  margin-bottom: 0.75rem;
+}
+
+.table-of-contents li:last-child {
+  margin-bottom: 0;
+}
+
+.table-of-contents a {
+  color: var(--primary-text-color);
+  text-decoration: none;
+  transition: color 0.2s;
+  display: inline-block;
+}
+
+.table-of-contents a:hover {
+  color: var(--nnt-orange);
+  text-decoration: none;
+}
+
+.table-of-contents>ul>li>a {
+  font-weight: 500;
 }
 </style>
--- layouts/toc.vue
+++ layouts/toc.vue
@@ -12,11 +12,11 @@
     <Banner :img="page.banner.img" v-if="page?.banner">
       <h1>{{ page.banner.text }}</h1>
     </Banner>
-    <div class="content">
-      <TableOfContents :page class="toc" />
+    <div class="content-wrapper">
       <ContentPage class="page">
         <slot />
       </ContentPage>
+      <TableOfContents :page class="toc" />
     </div>
     <Footer />
   </div>
@@ -29,15 +29,41 @@ defineProps({
 </script>
 
 <style scoped>
+.content-wrapper {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 2rem;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 2rem;
+}
+
 .toc {
+  display: none;
   position: sticky;
-  top: 0;
-  padding: 2rem;
+  top: 4rem;
+  height: fit-content;
+  margin-top: 4rem;
   background: var(--color-primary);
   color: var(--color-white);
   border-radius: 0.5rem;
-  margin-left: auto;
-  width: auto;
-  height: 0;
+}
+
+@media (min-width: 768px) {
+  .content-wrapper {
+    grid-template-columns: 1fr 300px;
+  }
+
+  .toc {
+    display: block;
+  }
+
+  .page {
+    padding-right: 0rem;
+  }
+}
+
+.page {
+  width: 100%;
 }
 </style>
