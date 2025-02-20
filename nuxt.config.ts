b75511b5161b9311812586679a92823b8d6ceba1
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2025-02-20 01:14:37 +0000

    Enable server-side rendering for /login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
 
   routeRules: {
     '/**': { prerender: true }, // TEMP: Prerender all routes - will need to be more selective in future
+    '/login': { ssr: true },
     '/mailing-list': { redirect: 'https://newtheatre.us3.list-manage.com/subscribe?u=ce5311ce46fe45638f90f4022&id=97e4899eb8' }
   },
 
