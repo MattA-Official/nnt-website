e9717095794dfdf2329c509eb81c371cc642f191
Matt Adcock <43476212+MattA-Official@users.noreply.github.com>
2025-03-03 14:47:17 +0000

    Prerendering is broken

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,7 @@ export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxt/image', '@nuxt/fonts', '@nuxtjs/seo', '@nuxt/icon', 'nuxt-vuefire'],
 
   routeRules: {
-    '/**': { prerender: true },
-    '/api/**': { prerender: false },
-    '/admin/**': { prerender: false },
-    '/account/**': { prerender: false },
-    // TODO: figure out which routes need to be excluded from prerendering
+    // TODO: figure out increasing performance using prerendering
     '/mailing-list': { redirect: 'https://newtheatre.us3.list-manage.com/subscribe?u=ce5311ce46fe45638f90f4022&id=97e4899eb8' },
   },
 
